d9d2fe852c0489f04195b2cb54c13a72dd1147d9
K0tok <jnkl@gmail.com>
2023-04-12 20:01:22 +0500

    2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,149 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="foldersList">
+      <div 
+      v-for="(folder, index) in folders" 
+      :key="index" 
+      class="folder" 
+      >
+        <div>
+
+        </div>
+      </div>
+      <div class="folder" @click="newFolder">
+        + Добавить папку
+      </div>
+      <div v-if="folderForm === true" class="folderForm">
+        <input v-model="folder.folderName" style="width: 80%; ">
+
+        <div class="circles">
+          <div class="circle red" :class="{'active': activeColor === 'red'}" @click="activeColor = 'red'"></div>
+          <div class="circle orange"></div>
+          <div class="circle pink"></div>
+          <div class="circle green"></div>
+          <div class="circle blue"></div>
+          <div class="circle purple"></div>
+          <div class="circle black"></div>
+        </div>
+        <button class="addFolderButton" @click="addFolder">Добавить</button>
+      </div>
+    </div>
+    <div class="taskList">
+        <!-- <div v-if="tasks.length === 0">
+            Задачи отсутствуют
+        </div> -->
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data() {
+      return {
+        folders: [],
+        folderForm: false,
+        folder: {
+          folderName: '',
+          folderColor: '',
+          tasks: [{
+
+          }]
+        },
+        activeColor: '',
+      }
+    },
+    methods: {
+      newFolder(){
+        this.folderForm = true
+      },
+      addFolder(){
+        this.folders.push(this.folder)
+        this.folderName = ''
+        this.folderColor = ''
+        this.folderForm = false
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body{
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+.app{
+  height: 100%;
+  background-color: white;
+
+  display: flex;
+  flex-direction: row;
+}
+.foldersList {
+  width: 30%;
+  height: 100%;
+  background-color: #F4F6F8;
+  border-right: 1px solid #F1F1F1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.folder{
+  width: 80%;
+  margin: 10px;
+  padding: 5px;
+  font-size: 30px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  border: 3px solid black;
+  border-radius: 10px;
+}
+.folderForm{
+  width: 80%;
+  height: 100px;
+  font-size: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 3px solid black;
+  border-radius: 10px;
+}
+.circles{
+  display: flex;
+  width: 80%;
+  justify-content: space-between;
+}
+.circle{
+  width: 30px; 
+  height: 30px; 
+  border-radius: 50%; 
+  border: 1px solid rgb(104, 104, 104);
+}
+.red{
+  background-color: rgb(255, 70, 70); 
+}
+.green{
+  background-color: rgb(84, 179, 84); 
+}
+.orange{
+  background-color: rgb(241, 176, 55); 
+}
+.pink{
+  background-color: rgb(255, 136, 156); 
+}
+.blue{
+  background-color: rgb(81, 81, 255); 
+}
+.purple{
+  background-color: rgb(168, 63, 168); 
+}
+.black{
+  background-color: black; 
+}
+.active {
+  border: 1px solid black;
 }
 </style>
